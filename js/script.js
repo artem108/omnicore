967d50e677c22061ca7e03bb5cfb4d272cdda23b
artem108 <artem.arshaniy108@gmail.com>
2017-05-29 21:48:11 +0300

    after session

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// var block = document.getElementsByClassName('section-new-arrivals');
+// var blockItem = block.getElementsByTagName('div');
+// console.log(blockItem);
+    // blockItem.onmouseover = function() {
+    //
+    //     console.log("gfredxs");
+    //
+    //   }
+// var a = document.getElementsByClassName('section-new-arrivals-items');
+// console.log(a);
